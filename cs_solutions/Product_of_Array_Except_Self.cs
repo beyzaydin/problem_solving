1ddf737bcb5304ba5cbd4fe56d1f889e145be5df
dogan <dogan.bacaksiz@gmail.com>
2023-06-02 22:16:29 +0300

    Product_of_Array_Except_Self.cs update

--- cs_solutions/Product_of_Array_Except_Self.cs
+++ cs_solutions/Product_of_Array_Except_Self.cs
@@ -1,15 +1,15 @@
 public class Solution {
     public int[] ProductExceptSelf(int[] nums) {
-        var result = new int[nums.Length];
-        int num = 1, i;
+        int[] result = new int[nums.Length];
+        int num, i;
 
-        for (i = 0; i < nums.Length; i++)
+        for (num = 1, i = 0; i < nums.Length; i++)
         {
             result[i] = num;
             num *= nums[i];
         }
 
-        for (i = nums.Length - 1,num = 1; i >= 0; i--)
+        for (num = 1, i = nums.Length - 1; i >= 0; i--)
         {
             result[i] *= num;
             num *= nums[i];
